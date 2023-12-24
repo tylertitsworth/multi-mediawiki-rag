61d4c47e7e5637234d0c01d1161271edb871a746
tylertitsworth <tyler.titsworth@intel.com>
2023-12-24 10:59:04 -0800

    adjust imports on unit tests

--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
+from main import MultiWiki, create_chain, create_vector_db
+
 import argparse
-import main
 import pytest
 
 
 def test_multiwiki():
-    wiki = main.MultiWiki()
+    wiki = MultiWiki()
     assert wiki.embeddings_model == "sentence-transformers/all-mpnet-base-v2"
     assert wiki.model == "volo"
     assert wiki.prompt == "What is a Tako?"
@@ -13,32 +14,29 @@ def test_multiwiki():
         "dnd5e": "",
         "eberron": "",
         "forgottenrealms": "",
-        "planescape": ""
+        "planescape": "",
     }
-    
+
+
 def test_multiwiki_set_args():
-    wiki = main.MultiWiki()
+    wiki = MultiWiki()
     parser = argparse.ArgumentParser()
-    parser.add_argument('--no-embed', dest='embed', action='store_false')
+    parser.add_argument("--no-embed", dest="embed", action="store_false")
     wiki.set_args(parser.parse_args([]))
     print(wiki.args)
     assert wiki.args.embed == True
 
+
 @pytest.mark.embed
 def test_create_vector_db():
-    main.create_vector_db(
-        "sentence-transformers/all-mpnet-base-v2",
-        "./sources",
-        {"forgottenrealms": ""}
+    create_vector_db(
+        "sentence-transformers/all-mpnet-base-v2", "./sources", {"forgottenrealms": ""}
     )
+
 
 @pytest.mark.ollama
 def test_create_chain():
-    chain = main.create_chain(
-        "sentence-transformers/all-mpnet-base-v2",
-        "volo"
-    )
+    chain = create_chain("sentence-transformers/all-mpnet-base-v2", "volo")
     res = chain("What is a Tako?")
     assert res["answer"] != ""
     assert res["source_documents"] != []
-

f434b7ea617c92ada633f4bef3476b100311bbe7
tylertitsworth <tyler.titsworth@intel.com>
2024-01-11 21:10:54 -0800

    lint ts

--- cypress/e2e/on_chat_start/spec.cy.ts
+++ cypress/e2e/on_chat_start/spec.cy.ts
@@ -1,11 +1,11 @@
 describe('on_chat_start', () => {
-    before(() => {
-        cy.visit('/');
-    });
-    it('should correctly run on_chat_start', () => {
-        const messages = cy.get('.step');
-        messages.should('have.length', 1);
+  before(() => {
+    cy.visit('/')
+  })
+  it('should correctly run on_chat_start', () => {
+    const messages = cy.get('.step')
+    messages.should('have.length', 1)
 
-        messages.eq(0).should('contain.text', 'Ah my good fellow!');
-    });
+    messages.eq(0).should('contain.text', 'Ah my good fellow!')
+  });
 });

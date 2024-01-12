8faecde4207585a0f9f013062d9e562b8c424ec8
Tyler Titsworth <tyler.titsworth@intel.com>
2024-01-11 21:58:42 -0800

    Update Linter (#36)
    * make test.sh executable
    
    * set ts linting
    
    * only run unit test on main.py change
    
    * lint ts
    
    * lint ts 2
    
    * remove lint rules
    
    * add npm to dependabot

--- cypress/e2e/set_chat_settings/spec.cy.ts
+++ cypress/e2e/set_chat_settings/spec.cy.ts
@@ -1,47 +1,46 @@
 describe('set_chat_settings', () => {
-    before(() => {
-        cy.visit('/');
-    });
+  before(() => {
+    cy.visit('/')
+  })
 
-    it('should update inputs', () => {
+  it('should update inputs', () => {
+    // Open chat settings modal
+    cy.get('#chat-settings-open-modal').should('exist')
+    cy.get('#chat-settings-open-modal').click()
+    cy.get('#chat-settings-dialog').should('exist')
 
-        // Open chat settings modal
-        cy.get('#chat-settings-open-modal').should('exist');
-        cy.get('#chat-settings-open-modal').click();
-        cy.get('#chat-settings-dialog').should('exist');
+    // Update inputs
+    const sliderInput = '{upArrow}{upArrow}{upArrow}{upArrow}{upArrow}'
 
-        // Update inputs
-        const sliderInput = '{upArrow}{upArrow}{upArrow}{upArrow}{upArrow}'
+    cy.get('#num_sources').clear().type('5')
+    cy.get('#num_sources').should('have.value', '5')
 
-        cy.get('#num_sources').clear().type('5');
-        cy.get('#num_sources').should('have.value', '5');
+    cy.get('#temperature').clear().type(sliderInput)
+    cy.get('#temperature').should('have.value', '0.5')
 
-        cy.get('#temperature').clear().type(sliderInput);
-        cy.get('#temperature').should('have.value', '0.5');
+    cy.get('#repeat_penalty').clear().type(sliderInput)
+    cy.get('#repeat_penalty').should('have.value', '0.9')
 
-        cy.get('#repeat_penalty').clear().type(sliderInput);
-        cy.get('#repeat_penalty').should('have.value', '0.9');
+    cy.get('#top_k').clear().type(sliderInput)
+    cy.get('#top_k').should('have.value', '5')
 
-        cy.get('#top_k').clear().type(sliderInput);
-        cy.get('#top_k').should('have.value', '5');
+    cy.get('#top_p').clear().type(sliderInput)
+    cy.get('#top_p').should('have.value', '0.5')
 
-        cy.get('#top_p').clear().type(sliderInput);
-        cy.get('#top_p').should('have.value', '0.5');
+    cy.contains('Confirm').click()
 
-        cy.contains('Confirm').click();
+    cy.get('.step').should('have.length', 1)
 
-        cy.get('.step').should('have.length', 1);
+    // Check if inputs are updated
+    cy.get('#chat-settings-open-modal').click()
+    cy.get('#num_sources').should('have.value', '5')
+    cy.get('#temperature').should('have.value', '0.5')
+    cy.get('#repeat_penalty').should('have.value', '0.9')
+    cy.get('#top_k').should('have.value', '5')
+    cy.get('#top_p').should('have.value', '0.5')
 
-        // Check if inputs are updated
-        cy.get('#chat-settings-open-modal').click();
-        cy.get('#num_sources').should('have.value', '5');
-        cy.get('#temperature').should('have.value', '0.5');
-        cy.get('#repeat_penalty').should('have.value', '0.9');
-        cy.get('#top_k').should('have.value', '5');
-        cy.get('#top_p').should('have.value', '0.5');
-
-        // Check if modal is correctly closed
-        cy.contains('Cancel').click();
-        cy.get('#chat-settings-dialog').should('not.exist');
-    });
-});
+    // Check if modal is correctly closed
+    cy.contains('Cancel').click()
+    cy.get('#chat-settings-dialog').should('not.exist')
+  })
+})

1f48ef07eb84ca0c4a6d0efbbdac78f3b0e1de61
tylertitsworth <tyler.titsworth@intel.com>
2024-01-16 16:52:17 -0800

    add e2e tests

---
+++ cypress/e2e/post_query/spec.cy.ts
@@ -0,0 +1,14 @@
+describe('POST /query', () => {
+  before(() => {
+    cy.visit('/')
+    cy.wait(30000)
+    cy.get('#chat-input').type("How many eyestalks does a beholder have?{enter}")
+    cy.get('.playground-button').should('exist').click()
+  })
+  it('test prompt', () => {
+    cy.request({method: "POST", url: "http://localhost:8000/query", body: {"prompt": "How many eyestalks does a Beholder have?"}, failOnStatusCode: false}).then((response) => {
+      expect(response.body.answer).length.to.be.greaterThan(1)
+      expect(response.body.source_documents).length.to.be.greaterThan(1)
+    })
+  })
+})
